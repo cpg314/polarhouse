a085d856300f7694dea68f1a4d674b649600fa86
cpg314 <44120267+cpg314@users.noreply.github.com>
2024-05-13 23:04:48 +0200

    py: Caching

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -1,4 +1,9 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
+
+use log::*;
+use polarhouse::polars::prelude::*;
 use pyo3::{
     exceptions::{PyException, PyIOError},
     prelude::*,
@@ -6,20 +11,30 @@ use pyo3::{
 use pyo3_polars::PyDataFrame;
 
 #[pyclass]
-struct Client(klickhouse::Client);
+struct Client {
+    inner: klickhouse::Client,
+    cache: Option<PathBuf>,
+}
 
 #[pymethods]
 impl Client {
     #[staticmethod]
-    #[pyo3(signature = (address, database="default".into(), username="default".into(), password="".into()))]
+    #[pyo3(signature = (address, database="default".into(), username="default".into(), password="".into(), caching=false))]
     fn connect(
         py: Python,
         address: String,
         database: String,
         username: String,
         password: String,
+        caching: bool,
     ) -> PyResult<&PyAny> {
         let locals = pyo3_asyncio::TaskLocals::with_running_loop(py)?.copy_context(py)?;
+        let cache = caching.then(dirs::cache_dir).flatten().and_then(|c| {
+            let c = c.join("polarhouse");
+            std::fs::create_dir_all(&c).ok()?;
+            Some(c)
+        });
+
         pyo3_asyncio::tokio::future_into_py_with_locals(py, locals.clone(), async move {
             klickhouse::Client::connect(
                 address,
@@ -31,7 +46,7 @@ impl Client {
             )
             .await
             .map_err(|e| PyException::new_err(format!("Failed to connect to Clickhouse: {}", e)))
-            .map(Client)
+            .map(|inner| Client { inner, cache })
         })
     }
     fn get_df_query_file<'p>(&self, py: Python<'p>, filename: PathBuf) -> PyResult<&'p PyAny> {
@@ -42,12 +57,41 @@ impl Client {
     }
     fn get_df_query<'p>(&self, py: Python<'p>, query: String) -> PyResult<&'p PyAny> {
         let locals = pyo3_asyncio::TaskLocals::with_running_loop(py)?.copy_context(py)?;
-        let ch = self.0.clone();
+        let ch = self.inner.clone();
+        let cache = if let Some(cache) = &self.cache {
+            let mut s = DefaultHasher::new();
+            query.hash(&mut s);
+            let hash = s.finish();
+            let filename = cache.join(hash.to_string());
+            if filename.is_file() {
+                debug!("Reading from cache {:?}", filename);
+                let reader = std::fs::read(&filename)?;
+                let reader = std::io::Cursor::new(reader);
+                match ParquetReader::new(reader).finish() {
+                    Ok(df) => {
+                        return pyo3_asyncio::tokio::future_into_py(py, async move {
+                            Ok(PyDataFrame(df))
+                        });
+                    }
+                    Err(_) => {
+                        std::fs::remove_file(&filename)?;
+                    }
+                }
+            }
+            Some(filename)
+        } else {
+            None
+        };
         pyo3_asyncio::tokio::future_into_py_with_locals(py, locals.clone(), async move {
-            polarhouse::get_df_query(query, Default::default(), &ch)
+            let mut df = polarhouse::get_df_query(query, Default::default(), &ch)
                 .await
-                .map_err(|e| PyIOError::new_err(e.to_string()))
-                .map(PyDataFrame)
+                .map_err(|e| PyIOError::new_err(e.to_string()))?;
+            if let Some(cache) = cache {
+                debug!("Saving to cache {:?}", cache);
+                let mut file = std::fs::File::create(cache)?;
+                let _ = ParquetWriter::new(&mut file).finish(&mut df);
+            }
+            Ok(PyDataFrame(df))
         })
     }
 }
@@ -55,6 +99,7 @@ impl Client {
 #[pymodule]
 #[pyo3(name = "polarhouse")]
 fn polarhouse_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    pyo3_log::init();
     // It appears that enabling the string cache from Python has no effect.
     polarhouse::polars::enable_string_cache();
     m.add_class::<Client>()?;
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,16 @@
 import asyncio
+import os
+import shutil
+import logging
 import tempfile
 
 from polarhouse import Client
 
 
 async def main():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
     client = await Client.connect("localhost:9000")
 
     # Query from string
@@ -19,5 +25,14 @@ async def main():
     df2 = await client.get_df_query_file(f.name)
 
     assert df.equals(df2)
+
+    # Caching
+    shutil.rmtree(os.path.expanduser("~/.cache/polarhouse"))
+    client = await Client.connect("localhost:9000", caching=True)
+    for i in range(2):
+        query = "SELECT * from superheroes"
+        df2 = await client.get_df_query(query)
+        assert df.equals(df2)
+
 
 asyncio.run(main())
